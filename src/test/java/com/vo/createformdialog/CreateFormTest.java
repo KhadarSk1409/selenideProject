88dd4594fb52b1038980f20ba55b0d6494ff25ca
Hrishikesh <hrishikesh.moghe@gogox.com>
2021-03-26 16:49:36 +0530

    Comitting change for CreateFormTest file, for help text in German

--- src/test/java/com/vo/createformdialog/CreateFormTest.java
+++ src/test/java/com/vo/createformdialog/CreateFormTest.java
@@ -4,6 +4,8 @@ import com.codeborne.selenide.SelenideElement;
 import com.vo.BaseTest;
 import org.apache.logging.log4j.core.util.Assert;
 import org.junit.jupiter.api.*;
+
+import static com.codeborne.selenide.CollectionCondition.texts;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -58,21 +60,39 @@ public class CreateFormTest extends BaseTest {
     @DisplayName("Verify that Title and ID fields are mandatory in Create form wizard")
     @Order(4)
     public void verifyMandatoryFieldsInFormWizard()  {
+
+        //Cancel the form creation wizard
+        $("#wizard-cancelButton").shouldBe(enabled).click(); //Cancel button in Create Form Wizard
+
+        $("#confirmation-dialog-title").should(exist);
+        $("#btnConfirm").shouldBe(enabled).click();  //Confirm the Cancellation
+        $("#btnCreateForm").shouldBe(enabled); //User is on Dashboard page
+        $("#user").waitUntil(visible, 5000).click(); //Wait until the 'User' element is visible on Dashboard and click on it
+        $("#myPreferences").click(); //Click on preferences
+
+        $("#account_settings.MuiListItem-button").shouldBe(visible).click(); //Account Settings
+
+        String existingAppLanguage = $("#defaultLocale").should(exist).getText(); //Fetch the existing value in Language dropddown
+
+
+        $("#toDashboard").click(); //Click on Home button
+        $("#btnCreateForm").should(exist).click(); //Verify that user is on Dashboard page and click on Create form
         $("#wizard-formHelp").should(exist).setValue("xyz1"); //Enter text in description field
-      //  $("#wizard-formUrl").should(exist).setValue("https://fireo.net/Dashboard/"); //Enter Direct link in Dashboard field
+        //  $("#wizard-formUrl").should(exist).setValue("https://fireo.net/Dashboard/"); //Enter Direct link in Dashboard field
 
-        $("#wizard-formTitle-helper-text").should(appear).shouldHave(text("Please insert the form title")); //Verify the Error shown below Title field - "Please insert the form title"
-
+        if (existingAppLanguage.contains("English"))
+            $("#wizard-formTitle-helper-text").should(appear).shouldHave(text("Please insert the form title")); //Verify the Error shown below Title field - "Please insert the form title"
+        else
+            $("#wizard-formTitle-helper-text").should(appear).shouldHave(text("Bitte geben Sie den Titel des Formulars ein")); //Verify the Error shown below Title field in German
         $("#wizard-createFormButton").shouldBe(disabled); //Create Form button should be disabled since Title field is blank
 
         $("#wizard-formHelp").should(exist).doubleClick().sendKeys(Keys.BACK_SPACE); //Clear text in description field
-      //  $("#wizard-formUrl").should(exist).setValue(""); //Clear Direct link in Dashboard field
 
         $("#wizard-formTitle").should(exist).click();
         $("#wizard-formTitle").setValue("This is the form Title"); //Enter value in Title field
 
         $("#wizard-formId").should(exist).doubleClick().sendKeys(Keys.BACK_SPACE); //Clearing the value in ID field
-        $("#wizard-formId").shouldHave(exactValue(""));
+        $("#wizard-formId").should(exist).shouldHave(exactValue(""));
 
         $("#wizard-createFormButton").shouldBe(disabled); //Create Form button should be disabled
 
@@ -121,7 +141,7 @@ public class CreateFormTest extends BaseTest {
         newString = string150characters + "s";
 
         $("#wizard-formHelp").doubleClick().sendKeys(Keys.BACK_SPACE); //Clear the Description field
-        $("#wizard-formHelp").setValue(newString); //Try to set 151 characters in the Description field
+        $("#wizard-formHelp").should(exist).setValue(newString); //Try to set 151 characters in the Description field
         $("#wizard-formHelp").shouldNotHave(exactValue(newString)); //New string with 151st character should not be present
         $("#wizard-formHelp").shouldHave(exactValue(string150characters)); //Only the string with 150 characters should be there
 
