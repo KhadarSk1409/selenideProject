d51e4039a8da1d6fee37a3697d0251b3a612b718
Hrishikesh <hrishikesh.moghe@gogox.com>
2021-04-19 23:25:03 +0530

    Committing changes for VLR-222 -Create Form Wizard - Page 02 - Language Properties

--- src/test/java/com/vo/createformdialog/LanguageProperties.java
+++ src/test/java/com/vo/createformdialog/LanguageProperties.java
@@ -3,12 +3,14 @@ package com.vo.createformdialog;
 import com.codeborne.selenide.SelenideElement;
 import com.vo.BaseTest;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import java.util.List;
 import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byAttribute;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
@@ -48,7 +50,7 @@ public class LanguageProperties extends BaseTest {
         cellsInFirstRow.get(3).shouldHave(value("true")); //Default button is checked for first row - English - German
 
         SelenideElement SecondRow = $$("#wizardFormDlg tbody tr").get(1);
-        List<SelenideElement> cellsInSecondRow = SecondRow.$$("td"); //Cells in the first row
+        List<SelenideElement> cellsInSecondRow = SecondRow.$$("td"); //Cells in the second row
         cellsInSecondRow.get(0).shouldHave(text("German - Germany"));
         cellsInSecondRow.get(1).shouldHave(text(formTitle)); //Form Title should have the same text as Title in Create Form
         cellsInSecondRow.get(2).shouldHave(text(formDesc)); //Form Description text should be same as Description in Create form
@@ -60,20 +62,25 @@ public class LanguageProperties extends BaseTest {
     @DisplayName("Verify Delete Language in Additional Language screen")
     @Order(2)
     public void verifyDeleteLanguage() {
-        //  $(By.xpath("//tbody/tr[1]/td[5]/div[1]/span[1]")).shouldBe(disabled); //Delete button should be disabled for Default language -> Not working??
-        $(By.xpath("//tbody/tr[2]/td[5]/div[1]/button[1]/span[1]/span[1]")).should(exist).click(); //Click on Edit button for German - German
-        $(By.xpath("//tbody/tr[2]/td[5]/div[1]/button[2]/span[1]")).should(exist).click(); //Click on Cancel Edit button for German - German
+        SelenideElement SecondRow = $$("#wizardFormDlg tbody tr").get(1); //Row for German - German
+        List<SelenideElement> cellsInSecondRow = SecondRow.$$("td"); //Cells in the second row
+        cellsInSecondRow.get(4).$(byAttribute("title", "Edit")).should(exist).click(); //Click on edit button for German - German
+        cellsInSecondRow.get(4).$(byAttribute("title", "Cancel")).should(exist).click(); //Cancel the Edit
+        cellsInSecondRow.get(4).$(byAttribute("title", "Edit")).should(exist); //User is back on the previous page
+        cellsInSecondRow.get(1).click(); //Click on Screen
 
-        $(By.xpath("//tbody/tr[2]/td[5]/div[1]/button[2]")).should(exist).click(); //Delete button for German - German
-        $(By.xpath("//h6[contains(text(),'Are you sure you want to delete this row?')]")).shouldHave(text("Are you sure you want to delete this row?"));
-        //Confirmation shown for deletion of secondary langauge
+        cellsInSecondRow.get(4).$(byAttribute("title", "Delete")).should(exist);
+        cellsInSecondRow.get(4).$(byAttribute("title", "Delete")).click(); //Delete button for German - German
+        cellsInSecondRow.get(0).shouldHave(text("Are you sure you want to delete this row?")); //Confirmation shown for deletion of secondary langauge
+        cellsInSecondRow.get(0).click(); //Click on Screen
+        cellsInSecondRow.get(1).$(byAttribute("title", "Cancel")).should(exist); //Cancel the Deletion
+        cellsInSecondRow.get(1).$(byAttribute("title", "Cancel")).click();
+        cellsInSecondRow.get(4).$(byAttribute("title", "Edit")).should(exist); //User is back on the previous page
+        cellsInSecondRow.get(1).click(); //Click on screen
+        cellsInSecondRow.get(4).$(byAttribute("title", "Delete")).should(exist);
+        cellsInSecondRow.get(4).$(byAttribute("title", "Delete")).click(); //Delete button for German - German
+        cellsInSecondRow.get(1).$(byAttribute("title", "Save")).click(); //Confirm the Deletion
 
-        $(By.xpath("//tbody/tr[2]/td[2]/div[1]/button[2]")).should(exist).click(); //Click on Cancel for Confirmation
-        $(By.xpath("//h6[contains(text(),'Are you sure you want to delete this row?')]")).shouldNot(exist); //The confirmation no longer exists
-
-        $(By.xpath("//tbody/tr[2]/td[5]/div[1]/button[2]")).should(exist).click(); //Delete button for German - German
-        $(By.xpath("//tbody/tr[2]/td[2]/div[1]/button[1]")).should(exist).click(); //Confirmation button for delete
-        $(By.xpath("//tbody/tr[2]/td[5]/div[1]/button[2]")).shouldNot(exist); //German - German is deleted
     }
 
     @Test
@@ -81,22 +88,30 @@ public class LanguageProperties extends BaseTest {
     @Order(3)
     public void verifyAddLanguage() {
         $(".mtable_toolbar button[title=\"Add\"]").should(exist).click(); //Click on + button
-        //   $(By.xpath("//tbody/tr[1]/td[4]/*[1]")).shouldBe(disabled); //The Tick in Default column is disabled -> This is failing ??
-        $(By.xpath("//tbody/tr[2]/td[4]/span[1]")).should(exist).shouldNotBe(checked); //The checkbox for newly added Germany-Germany
-        $(By.xpath("//tbody/tr[2]/td[1]/div[1]/*[1]")).click(); //Click on Language Dropdown
-        $(By.xpath("//body/div[@id='menu-']/div[3]/ul[1]/li[1]")).shouldHave(text("German - Germany")).click();
-        //  $(By.xpath("//tbody/tr[2]/td[2]/div[1]/input[1]")).shouldHave(text("Form Title")); //Form Title field after selecting Language -> Not working??
-        //    $(By.xpath("//tbody/tr[2]/td[3]/div[1]/input[1]")).shouldHave(text("Form Description")); //Form Description field after selecting Language -> Not working??
-        $(By.xpath("//tbody/tr[2]/td[5]/div[1]/button[1]/span[1]/*[1]")).shouldBe(enabled); //Save button/Check is enabled
-        $(By.xpath("//tbody/tr[2]/td[4]/span[1]/span[1]/input[1]")).should(exist).shouldNotBe(checked); //Default checkbox is originally unchecked
-        $(By.xpath("//tbody/tr[2]/td[4]/span[1]/span[1]/input[1]")).click(); //Check the chekbox for setting as Default language
+
+        //Define the table rows
+        SelenideElement secondRow = $$("#wizardFormDlg tbody tr").get(1);
+        List<SelenideElement> cellsInSecondRow = secondRow.$$("td"); //Cells in the first row
+
+        cellsInSecondRow.get(3).should(exist).shouldNotBe(checked); //The checkbox for newly added Germany-Germany
+
+        cellsInSecondRow.get(0).click(); //Click on Language Dropdown
+        $(By.xpath("//body/div[@id='menu-']/div[3]/ul[1]/li[1]")).shouldHave(text("German - Germany")).click(); //->German - Germany dropdown menu, id??
+        //cellsInSecondRow.get(1).shouldHave(text("Form Title")); -> Not working ??
+        //cellsInSecondRow.get(2).shouldHave(text("Form Description")); -> Not working ??
+        cellsInSecondRow.get(4).shouldBe(enabled); //Save button/Check is enabled
+        cellsInSecondRow.get(3).shouldNotBe(checked); //Default checkbox is originally unchecked
+        cellsInSecondRow.get(3).click(); //Check the chekbox for setting as Default language
         // Note: After selecting German as Default language and saving, only English - Great Britain is shown-> ??
+        cellsInSecondRow.get(4).shouldBe(enabled).click(); //Click on Save tick
 
-        $(By.xpath("//tbody/tr[2]/td[5]/div[1]/button[1]/span[1]/*[1]")).shouldBe(enabled).click(); //Click on Save tick
-
-        $(By.xpath("//tbody/tr[1]/td[5]/div[1]/button[1]/span[1]/span[1]")).should(exist).click(); //Click on the Edit button
-        $(By.xpath("//tbody/tr[1]/td[2]/div[1]/input[1]")).shouldBe(enabled); //Form Title is now enabled
-        $(By.xpath("//tbody/tr[1]/td[3]/div[1]/input[1]")).shouldBe(enabled); //Form Description is now enabled
+        SelenideElement firstRow = $$("#wizardFormDlg tbody tr").get(0);
+        List<SelenideElement> cellsInFirstRow = firstRow.$$("td"); //Cells in the first row
+        cellsInFirstRow.get(0).click(); //Click on screen
+        cellsInFirstRow.get(4).$(byAttribute("title", "Edit")).should(exist);
+        cellsInFirstRow.get(4).$(byAttribute("title", "Edit")).click(); //Click on the Edit button
+        cellsInFirstRow.get(1).shouldBe(enabled); //Form Title is now enabled
+        cellsInFirstRow.get(1).shouldBe(enabled); //Form Description is now enabled
         $("#wizard-cancelButton").click(); //Click on Cancel button
         $("#confirmation-dialog-title").should(exist); //Confirmation for Cancellation is shown
         $("#confirmation-dialog-content").should(exist).click(); //Click on Confirm button
