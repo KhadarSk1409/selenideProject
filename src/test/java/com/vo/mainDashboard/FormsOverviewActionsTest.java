803658126f7333506394432c3f463df549afd6e2
Nikolai Raitsev <nikolai.raitsev@gmail.com>
2021-05-10 16:36:44 +0200

    VLR-268 example how to use consice element selection

--- src/test/java/com/vo/mainDashboard/FormsOverviewActionsTest.java
+++ src/test/java/com/vo/mainDashboard/FormsOverviewActionsTest.java
@@ -20,53 +20,45 @@ import static com.codeborne.selenide.Selenide.$$;
 public class FormsOverviewActionsTest extends BaseTest {
 
     @Test
-    @DisplayName("Identification of Actions on Form OverviewTable")
+    @DisplayName("Actions on Form Overview Table should be enabled or disabled depending of form state")
     @Order(1)
     public void IdentificationOfActionsInFormOverviewTable() {
-
-        $("#formRelatedTabsCard .vo-expand-collapse").click();
-        $("#formRelatedTabsCard .vo-expand-collapse").should(have(attribute("title", "Collapse")));
-
         SelenideElement table = $("#formRelatedTabsCard .MuiTableBody-root").shouldBe(visible);
-        List<WebElement> rows = table.findElements(By.tagName("tr"));
+        ElementsCollection rows = table.$$("tr");
         System.out.println(" Form Count is " + rows.size());
 
-        if (rows.size() >= 0) {
-            for (int i = 2; i <= rows.size(); i++) {
-                String Forms = $("#formListTable tr:nth-child(" + i + ") td:nth-child(3) div span").getText();
-                String createdBy = $("#formListTable tr:nth-child(" + i + ") td:nth-child(4)").getText();
-
-                if (Forms.equals("Published") && createdBy.equals("GUI Tester")) {
-                    $("#formListTable tr:nth-child(" + i + ") td:nth-child(7) span:nth-child(1) span.MuiIconButton-label span.MuiIconButton-label span").shouldBe(enabled);
-                    //Fill Form should be enabled
-                    $("#formListTable tr:nth-child(" + i + ") td:nth-child(7) span:nth-child(2) span.MuiIconButton-label span.MuiIconButton-label span").shouldBe(enabled);
-                    //Open form in form designer should be enabled
-                    $("#formListTable tr:nth-child(" + i + ") td:nth-child(7) span:nth-child(3) span.MuiIconButton-label span.MuiIconButton-label span").shouldBe(enabled);
-                    //Open form Dashboard should be enabled
-                    $("#formListTable tr:nth-child(" + i + ") td:nth-child(7) span:nth-child(4) span.MuiIconButton-label span.MuiIconButton-label span").shouldBe(enabled);
-                    //Add favorite should be enabled
-                } else if (Forms.equals("Published/in draft") && createdBy.equals("GUI Tester")) {
-                    $("#formListTable tr:nth-child(" + i + ") td:nth-child(7) span:nth-child(1) span.MuiIconButton-label span.MuiIconButton-label span").shouldBe(enabled);
-                    $("#formListTable tr:nth-child(" + i + ") td:nth-child(7) span:nth-child(2) span.MuiIconButton-label span.MuiIconButton-label span").shouldBe(enabled);
-                    $("#formListTable tr:nth-child(" + i + ") td:nth-child(7) span:nth-child(3) span.MuiIconButton-label span.MuiIconButton-label span").shouldBe(enabled);
-                    $("#formListTable tr:nth-child(" + i + ") td:nth-child(7) span:nth-child(4) span.MuiIconButton-label span.MuiIconButton-label span").shouldBe(enabled);
-                } else {
-                    $("#formListTable tr:nth-child(" + i + ") td:nth-child(7) span:nth-child(1) button span div button").shouldBe(disabled);
-                    //Fill Form should be disabled
-                    $("#formListTable tr:nth-child(" + i + ") td:nth-child(7) span:nth-child(2) button span div button").shouldBe(enabled);
-                    //Open form in form designer should be enabled
-                    $("#formListTable tr:nth-child(" + i + ") td:nth-child(7) span:nth-child(3) button span div button").shouldBe(disabled);
-                    //Open form Dashboard should be disabled
-                }
-                }
-
-            }
-                    else {
-                    System.out.println("No Forms available");
-
+        if (rows.size() == 0) {
+            System.out.println("No Forms available");
+            return;
         }
 
+        rows.forEach(rowEl -> {
+            String formState = rowEl.$("td:nth-child(3)").getText();
+            String createdBy = rowEl.$("td:nth-child(4)").getText();
+
+            if (formState.equals("Published") && createdBy.equals("GUI Tester")) {
+                //Fill Form should be enabled
+                rowEl.$(".fa-edit").closest("button").shouldBe(enabled);
+                //Open form in form designer should be enabled
+                rowEl.$(".fa-pen").closest("button").shouldBe(enabled);
+                //Open form Dashboard should be enabled
+                rowEl.$(".fa-chart-area").closest("button").shouldBe(enabled);
+                //Add favorite should be enabled
+                rowEl.$(".favorite").closest("button").shouldBe(enabled);
+            } else if (formState.equals("Published/in draft") && createdBy.equals("GUI Tester")) {
+                rowEl.$(".fa-edit").closest("button").shouldBe(enabled);
+                rowEl.$(".fa-pen").closest("button").shouldBe(enabled);
+                rowEl.$(".fa-chart-area").closest("button").shouldBe(enabled);
+                rowEl.$(".favorite").closest("button").shouldBe(enabled);
+            } else {
+                rowEl.$(".fa-edit").closest("button").shouldBe(disabled);
+                rowEl.$(".fa-pen").closest("button").shouldBe(enabled);
+                rowEl.$(".fa-chart-area").closest("button").shouldBe(disabled);
+                rowEl.$(".favorite").shouldNotBe(visible);
+            }
+        });
     }
+
         @Test
         @DisplayName("Navigation Actions in Forms Overview Table")
         @Order(2)
