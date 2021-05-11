e38455782cc91ecbb1ea44905ef7b7c570766cb3
Hrishikesh <hrishikesh.moghe@gogox.com>
2021-05-11 13:46:02 +0530

    Committing changes for VLR-271 Area Header

--- src/test/java/com/vo/mainDashboard/AreaHeaderTest.java
+++ src/test/java/com/vo/mainDashboard/AreaHeaderTest.java
@@ -1,15 +1,17 @@
 package com.vo.mainDashboard;
 
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.*;
+import com.codeborne.selenide.impl.WebDriverContainer;
 import com.vo.BaseTest;
 import org.junit.jupiter.api.*;
 import java.util.List;
-import static com.codeborne.selenide.Condition.appear;
-import static com.codeborne.selenide.Condition.exist;
+import java.lang.*;
+
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byCssSelector;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.WebDriverRunner.url;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
@@ -18,37 +20,29 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class AreaHeaderTest extends BaseTest {
 
     @Test
-    @DisplayName("Getting to Form Dashboard - OPTION 01")
+    @DisplayName("Navigate to Form Dashboard from Favorites")
     @Order(1)
-    public void gettingToFormDashboardOption1() {
+    public void navigateToFormDashboardFromFavorites() {
         //  List<SelenideElement> listFavForms = $("#root").$$(byClassName("MuiCollapse-wrapperInner")); //Fetch the Favorite forms list elements
         $("#btnCreateForm").should(exist);
-        $("#root div.MuiCollapse-container.MuiCollapse-entered nav  > div > div").should(exist);
-        List<SelenideElement> listFavForms = $$("#root div.MuiCollapse-container.MuiCollapse-entered nav  > div > div");
-        int listFavFormsCount = listFavForms.size();
-        assertTrue((listFavFormsCount > 1), "Verify that Favorite forms are available"); //Verify that Favorite form is available
-
-        for (int i = 0; i < listFavFormsCount; i++) {
-            String formName = listFavForms.get(i).getText();
-            if (formName.contains("NR Test Form")) //The array element should not be NR Test form
-                continue;
-            else
-                listFavForms.get(i).click(); //Click on the Favorite form
-            $("#full-width-tabpanel-MY_DATA").should(exist); //Navigated to Form Dashboard
-            break;
-        }
+        $("#navFavoriteFormsItems .MuiListItem-root").should(exist);
+        ElementsCollection listFavForms = $$("#navFavoriteFormsItems .MuiListItem-root");
+        listFavForms.shouldHave(CollectionCondition.sizeGreaterThan(1)); //Verify that Favorite form is available
+        listFavForms.get(0).click(); //Click on the Favorite form
+        $("#full-width-tabpanel-MY_DATA").should(exist); //Navigated to Form Dashboard
 
     }
 
     @Test
-    @DisplayName("Getting to Form Dashboard - OPTION 02")
+    @DisplayName("Navigate to Form Dashboard from Form Table")
     @Order(2)
-    public void gettingToDashboardOption2() {
+    public void navigateToDashboardFormTable() {
         String getFormName = $("#formDashboardHeaderLeft h6").should(exist).getText();
         $("#toDashboard").should(exist).click(); //Click on Launchpad
         $("#btnCreateForm").should(exist); //Verify that Create form button is available
         $("#formListTable table tbody tr").should(exist);
-        List<SelenideElement> formRows = $$("#formListTable table tbody tr"); //Fetch form rows
+       // List<SelenideElement> formRows = $$("#formListTable table tbody tr"); //Fetch form rows
+        ElementsCollection formRows = $$("#formListTable table tbody tr"); //Fetch form rows
         int formRowsSize = formRows.size();
         for (int i = 0; i < formRowsSize; i++) {
             String formName = formRows.get(i).$(byCssSelector("td:nth-child(2)")).getText(); //Select form name for each row
@@ -56,7 +50,8 @@ public class AreaHeaderTest extends BaseTest {
 
             if (formName.contentEquals(getFormName)) {
                 formRows.get(i).$(byCssSelector("td:nth-child(1)")).click(); //Click on '>' Open Form Dashboard
-                formRows.get(i).$(byCssSelector("td:nth-child(7) span:nth-child(3)")).should(exist).click();
+                formRows.get(i).$(byCssSelector("button .fa-chart-area")).should(exist).click();
+               // formRows.get(i).$(byCssSelector("td:nth-child(7) span:nth-child(3)")).should(exist).click();
                 $("#full-width-tabpanel-MY_DATA").should(exist); //Navigated to Form Dashboard
                 $("#formDashboardHeaderLeft h6").should(exist).getText().contentEquals(getFormName); //Navigated to the Form Dashboard for the form
                 break;
@@ -83,13 +78,13 @@ public class AreaHeaderTest extends BaseTest {
     @Test
     @DisplayName("Validate Form Data Card")
     @Order(4)
-    @Disabled
     public void validateTheFormDataCard() {
         $("#formDashboardHeaderLeft button").should(exist).click(); //The icon i for Data Card
         $("#cUsageOverview").should(exist); //Data card is displayed
-//       $("#cUsageOverview").$(byAttribute("title","Copy URL to Clipboard")).click(); //Copy url ??
-        // String copyUrl = KeyEvent ??
-        // System.out.println("copyUrl : "+copyUrl);
+        String expectedUrl = url(); //Get the url of the browser
+        $("#cUsageOverview p i").should(exist).click(); //Click on copy url button in the Data card
+        String clipBoardUrl = Selenide.clipboard().getText();
+        assertTrue(clipBoardUrl.equals(expectedUrl));
     }
 
     @Test
@@ -110,18 +105,19 @@ public class AreaHeaderTest extends BaseTest {
         $("#btnEditFormDesign").should(exist).click(); //Edit Form Design
         $("#btnFormDesignPublish").should(exist); //Publish button to ensure user has navigated to Edit Form screen
         $("#btnCloseEditForm").should(exist).click(); //Click on x on Edit Form design
+        $("#btnCloseEditForm").should(disappear);
         $("#btnEditFormDesign").should(exist); //Edit Form Design to ensure that user has navigated back to Form Dashboard
     }
 
     @Test
     @DisplayName("Verify button notifications")
     @Order(7)
-    @Disabled
     public void verifyButtonNotifications() {
         $("#btnNotificationPopover").should(exist).click(); //Notifications icon
-        $("notificationsCard").should(exist).click(); //Notifications card and click on it
-        $("notificationsCard").should(exist); //Notifications card still exists
-        // $("body").click(); ?? Not wokring - user needs to click on the Form Dashboard page and come back
+        $("#notificationsCard").should(appear).click(); //Notifications card and click on it
+        $("#notificationsCard").should(appear); //Notifications card still exists
+        $("body").click(); //?? Not wokring - user needs to click on the Form Dashboard page and come back
+        $("#notificationsCard").should(disappear);
         $("#formDashboardHeaderAppBar .btnMoreOptionsMenu").should(appear); //Menu button to ensure that user has navigated back to Form Dashboard
     }
 
