1ca6202458c8fb5cebd283de0c82992b187317af
Shaik Khadhar <shaikh@visualorbit.com>
2021-05-11 19:26:35 +0530

    Committing changes for VLR-282

--- src/test/java/com/vo/mainDashboard/FormsOverviewActionsTest.java
+++ src/test/java/com/vo/mainDashboard/FormsOverviewActionsTest.java
@@ -20,7 +20,7 @@ import static com.codeborne.selenide.Selenide.$$;
 public class FormsOverviewActionsTest extends BaseTest {
 
     @Test
-    @DisplayName("Actions on Form Overview Table should be enabled or disabled depending of form state")
+    @DisplayName("Actions on Form Overview Table should be enabled or disabled depending on form state")
     @Order(1)
     public void IdentificationOfActionsInFormOverviewTable() {
         SelenideElement table = $("#formRelatedTabsCard .MuiTableBody-root").shouldBe(visible);
@@ -37,14 +37,14 @@ public class FormsOverviewActionsTest extends BaseTest {
             String createdBy = rowEl.$("td:nth-child(4)").getText();
 
             if (formState.equals("Published") && createdBy.equals("GUI Tester")) {
-                //Fill Form should be enabled
                 rowEl.$(".fa-edit").closest("button").shouldBe(enabled);
-                //Open form in form designer should be enabled
+                //Fill Form should be enabled
                 rowEl.$(".fa-pen").closest("button").shouldBe(enabled);
-                //Open form Dashboard should be enabled
+                //Open form in form designer should be enabled
                 rowEl.$(".fa-chart-area").closest("button").shouldBe(enabled);
-                //Add favorite should be enabled
+                //Open form Dashboard should be enabled
                 rowEl.$(".favorite").closest("button").shouldBe(enabled);
+                //Add favorite should be enabled
             } else if (formState.equals("Published/in draft") && createdBy.equals("GUI Tester")) {
                 rowEl.$(".fa-edit").closest("button").shouldBe(enabled);
                 rowEl.$(".fa-pen").closest("button").shouldBe(enabled);
@@ -59,55 +59,60 @@ public class FormsOverviewActionsTest extends BaseTest {
         });
     }
 
-        @Test
-        @DisplayName("Navigation Actions in Forms Overview Table")
-        @Order(2)
-        public void FormsNavigationActions () {
-            $("#formRelatedTabsCard .vo-expand-collapse").click();
-            $("#formRelatedTabsCard .vo-expand-collapse").should(have(attribute("title", "Expand")));
+    @Test
+    @DisplayName("Navigation Actions in Forms Overview Table")
+    @Order(2)
+    public void formsNavigationActions() {
+        SelenideElement table = $("#formRelatedTabsCard .MuiTableBody-root").shouldBe(visible);
 
-            SelenideElement table = $("#formRelatedTabsCard .MuiTableBody-root").shouldBe(visible);
-            List<WebElement> rows = table.findElements(By.tagName("tr"));
-            System.out.println(" Form Count is " + rows.size());
-            if (rows.size() >= 0) {
-            for (int i = 2; i <= rows.size(); i++) {
-                String Forms = $("#formListTable tr:nth-child(" + i + ") td:nth-child(3) div span").getText();
-                String createdBy = $("#formListTable tr:nth-child(" + i + ") td:nth-child(4)").getText();
+        ElementsCollection rows = table.$$("tr");
+        int rowsSize = rows.size();
+        System.out.println(" Form Count is " + rowsSize);
 
-                if (Forms.equals("Published") && createdBy.equals("GUI Tester")) {
-                    $("#formListTable tr:nth-child(" + i + ") td:nth-child(7) span:nth-child(1) span.MuiIconButton-label span.MuiIconButton-label span").shouldBe(enabled).click();
-                    //Click on Fill form
-                    $("#cDataCardActions").shouldBe(visible);
-                    $("#cDataCardActions").$("#btnCloseDataFillForm").click();
-                    $("#formListTable tr:nth-child(" + i + ") td:nth-child(7) span:nth-child(2) span.MuiIconButton-label span.MuiIconButton-label span").shouldBe(enabled).click();
-                    //Click on Open form in form designer
-                    $("#formtree_card").shouldBe(visible).shouldHave(text("Form structure"));
-                    $("#toDashboard").click();
-                    $("#formListTable tr:nth-child(" + i + ") td:nth-child(7) span:nth-child(3) span.MuiIconButton-label span.MuiIconButton-label span").shouldBe(enabled).click();
-                    //Click on Open form dashboard
-                    $("#cUsageOverview").should(appear);
-                    //$("#formDashboardHeaderLeft").should(appear).$("#btnCreateNewData").shouldBe(enabled);
-                    $("#toDashboard").click();
-                } else if (Forms.equals("Published/in draft") && createdBy.equals("GUI Tester")) {
-                    $("#formListTable tr:nth-child(" + i + ") td:nth-child(7) span:nth-child(1) span.MuiIconButton-label span.MuiIconButton-label span").shouldBe(enabled).click();
-                    $("#cDataCardActions").shouldBe(visible);
-                    $("#cDataCardActions").$("#btnCloseDataFillForm").click();
-                    $("#formListTable tr:nth-child(" + i + ") td:nth-child(7) span:nth-child(2) span.MuiIconButton-label span.MuiIconButton-label span").shouldBe(enabled).click();
-                    $("#formtree_card").shouldBe(visible).shouldHave(text("Form structure"));
-                    $("#toDashboard").click();
-                    $("#formListTable tr:nth-child(" + i + ") td:nth-child(7) span:nth-child(3) span.MuiIconButton-label span.MuiIconButton-label span").shouldBe(enabled).click();
-                    $("#cUsageOverview").should(appear);
-                    $("#toDashboard").click();
-                } else {
-                    $("#formListTable tr:nth-child(" + i + ") td:nth-child(7) span:nth-child(2) button span div button").shouldBe(enabled).click();
-                    //Click Open form in form designer
-                    $("#formtree_card").shouldBe(visible).shouldHave(text("Form structure"));
-                    $("#toDashboard").click();
-                }
+        if (rowsSize == 0) {
+            System.out.println("No Forms available");
+            return;
+        }
+
+        for (int i = 0; i <= rowsSize; i++) {
+            SelenideElement rowEl = $("#formListTable tbody tr:nth-child(" + i + ")");
+
+
+            String formState = rowEl.$("td:nth-child(3)").getText();
+            String createdBy = rowEl.$("td:nth-child(4)").getText();
+
+            if (formState.equals("Published") && createdBy.equals("GUI Tester")) {
+                rowEl.$(".fa-edit").closest("button").shouldBe(enabled).click(); //Click on Fill form
+                $("#cDataCardActions").shouldBe(visible);
+                $("#cDataCardActions").$("#btnCloseDataFillForm").click();
+                $("#formListTable").should(appear);
+                rowEl.$(".fa-pen").closest("button").shouldBe(enabled).click();//Click on Open form in form designer
+                $("#formtree_card").shouldBe(visible).shouldHave(text("Form structure"));
+                $("#toDashboard").click();
+                $("#formListTable").should(appear);
+                rowEl = $("#formListTable tbody tr:nth-child(" + i + ")");
+                rowEl.$(".fa-chart-area").closest("button").shouldBe(enabled).click();//Click on Open form dashboard
+                $("#toDashboard").click();
+            } else if (formState.equals("Published/in draft") && createdBy.equals("GUI Tester")) {
+                rowEl.$(".fa-edit").closest("button").shouldBe(enabled).click(); //Click on Fill form
+                $("#cDataCardActions").shouldBe(visible);
+                $("#cDataCardActions").$("#btnCloseDataFillForm").click();
+                $("#formListTable").should(appear);
+                rowEl.$(".fa-pen").closest("button").shouldBe(enabled).click();//Click on Open form in form designer
+                $("#formtree_card").shouldBe(visible).shouldHave(text("Form structure"));
+                $("#toDashboard").click();
+                $("#formListTable").should(appear);
+                rowEl = $("#formListTable tbody tr:nth-child(" + i + ")");
+                rowEl.$(".fa-chart-area").closest("button").shouldBe(enabled).click();//Click on Open form dashboard
+                $("#toDashboard").click();
+            } else {
+                rowEl.$(".fa-pen").closest("button").shouldBe(enabled).click();//Click on Open form in form designer
+                //Click Open form in form designer
+                $("#formtree_card").shouldBe(visible).shouldHave(text("Form structure"));
+                $("#toDashboard").click();
             }
         }
-                else{
-                    System.out.println("No Forms available");
-                }
     }
+
 }
+
