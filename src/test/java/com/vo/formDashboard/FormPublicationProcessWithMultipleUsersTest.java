60eb7eab913a319c38ec02f032113fa524ab79a1
Shaik Khadhar <shaikh@visualorbit.com>
2021-06-24 11:28:35 +0530

    Implemented Pull Request Comments for VLR-311

--- src/test/java/com/vo/formDashboard/FormPublicationProcessWithMultipleUsersTest.java
+++ src/test/java/com/vo/formDashboard/FormPublicationProcessWithMultipleUsersTest.java
@@ -1,7 +1,10 @@
 package com.vo.formDashboard;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
 import com.vo.BaseTest;
+import org.apache.commons.lang3.tuple.Pair;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
@@ -10,6 +13,7 @@ import org.junit.jupiter.api.TestMethodOrder;
 import static com.codeborne.selenide.CollectionCondition.itemWithText;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
+import static reusables.ReuseActions.createNewForm;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @DisplayName("Verify the form publication process with multiple users")
@@ -17,16 +21,13 @@ public class FormPublicationProcessWithMultipleUsersTest extends BaseTest {
 
     @Test
     @DisplayName("Verify Create a form and publish with multiple users")
-    public void publicationProcess()  {
+    public void publicationProcess() {
         shouldLogin(UserType.USER_01);
-        $("#btnCreateForm").should(exist).click(); //Click on Create New Form
-        $("#dlgFormFormWizard").should(appear);
-        $("#wizard-formTitle").should(exist).setValue("FORM PUBLICATION PROCESS"); //Enter the Title
-        $("#selFormLabelsControl .MuiChip-label").has(text("guitest"));
-        $("#selLabel ~ .MuiAutocomplete-endAdornment .MuiAutocomplete-popupIndicator").should(exist).click();
-        $$(".MuiAutocomplete-popper li").shouldHave(itemWithText("guitest"), 8000);
-        $$(".MuiAutocomplete-popper li").findBy(text("guitest")).click(); //Click on the selected Label
+        //Create a New Form
+        Pair<String, String> formName=createNewForm();
+        String actualFormName= formName.getKey();
         $("#wizard-createFormButton").should(exist).shouldBe(enabled).click(); //Click on Create Form
+        applyLabelForTestForms(); //Apply guitest label
         $("#formDashboardHeaderLeft").should(appear);
         $("#block-loc_en-GB-r_1-c_1").should(exist).click(); //Click on + to add a field
         $("#template_card").should(appear).$("#li-template-Textfield-04").click(); //Add one field
@@ -55,14 +56,29 @@ public class FormPublicationProcessWithMultipleUsersTest extends BaseTest {
         $("#form-publish-dialog").$("#btnConfirm").should(exist).shouldBe(enabled).click();
 
         shouldLogin(UserType.USER_02); //Should login as GUI Tester 02
-        $("#tasksCard tbody tr:nth-child(3) td:nth-child(3)")
-                .shouldHave(value("FORM PUBLICATION PROCESS")).should(exist);
-        $("#tasksCard  tbody tr:nth-child(3) td:nth-child(6) span:nth-child(2) .fa-check")
-                .should(exist).shouldBe(enabled).click(); //Click on Quick Approve
-        $("#tasksCard  tbody tr:nth-child(3) td:nth-child(6) span:nth-child(2)").shouldNot(exist);
+
+        SelenideElement table = $("#tasksCard .MuiTableBody-root").shouldBe(visible);
+
+        ElementsCollection rows = table.$$("tr");
+        System.out.println(" Form Count is " + rows.size());
+
+        if (rows.size() == 0) {
+            System.out.println("No Forms available");
+            return;
+        }
+
+        rows.forEach(rowEl -> {
+            String form = rowEl.$("td:nth-child(3)").getText();
+
+            if (form.equals(actualFormName)) {
+                rowEl.$(".fa-check").closest("button").should(exist).shouldBe(enabled).click();
+            }
+        });
 
         //Verify the form approved by GUI Tester 02 is Published or not
         shouldLogin(UserType.USER_01); //Should login as GUI Tester 01
+        $("#formListTable tbody tr:nth-child(1) td:nth-child(2)")
+                .shouldHave(value(actualFormName)).should(exist);
         $("#formListTable tbody tr:nth-child(1) td:nth-child(3)").should(exist)
                 .shouldHave(Condition.text("Published"));
 
