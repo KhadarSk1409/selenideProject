fca2e2ee3cffafb0f8d8d0e6170438f9640a9fc0
Shaik Khadhar <shaikh@visualorbit.com>
2021-12-23 15:57:13 +0530

    VLR-450 Refactored FormattedTextTest, FileUploadFieldTest and RadioGroupTest
    Did some necessary minor change in LabelFieldTest

--- src/test/java/com/vo/formdesign/FileUploadFieldTest.java
+++ src/test/java/com/vo/formdesign/FileUploadFieldTest.java
@@ -13,6 +13,7 @@ import org.openqa.selenium.Keys;
 
 import java.awt.event.KeyEvent;
 import java.io.IOException;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -23,6 +24,7 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static java.lang.Integer.parseInt;
 import static reusables.ReuseActions.createNewForm;
+import static reusables.ReuseActions.elementLocators;
 import static reusables.ReuseActionsFormCreation.*;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -70,11 +72,11 @@ public class FileUploadFieldTest extends BaseTest {
             String prevBlockId = "#block-loc_en-GB-r_" + (row - 1) + "-c_" + col;
             $(prevBlockId + " .add-row").shouldBe(visible).click();
         }
-        String initialVerNumStr = $("#formMinorversion").should(exist).getText(); //Fetch initial version
+        String initialVerNumStr = $(elementLocators("InitialVersion")).should(exist).getText(); //Fetch initial version
         $(blockId).shouldBe(visible).click();
-        $("#li-template-FileUploadField-04").should(exist).click();
-        $(blockId).$(".fa-pen").closest("button").shouldBe(visible).click(); //Click on Edit
-        $("#formMinorversion").shouldNotHave(text(initialVerNumStr)); //Verify that version has increased
+        $(elementLocators("FileUploadField")).should(exist).click();
+        $(blockId).$(elementLocators("PenIcon")).closest("button").shouldBe(visible).click(); //Click on Edit
+        $(elementLocators("InitialVersion")).shouldNotHave(text(initialVerNumStr)); //Verify that version has increased
 
 
         //Label
@@ -111,10 +113,10 @@ public class FileUploadFieldTest extends BaseTest {
 
             $(By.id(FileUploadFieldTest.FileUploadFieldIds.numberField_minCount.name())).should(exist); //Verify that Minimum count field exists
 
-            String initialVerNumStr2 = $("#formMinorversion").should(exist).getText(); //Fetch initial version
+            String initialVerNumStr2 = $(elementLocators("InitialVersion")).should(exist).getText(); //Fetch initial version
             selectAndClear(By.id(FileUploadFieldTest.FileUploadFieldIds.numberField_minCount.name()))
                     .setValue(text_numberField_minCount).sendKeys(Keys.TAB);
-            $("#formMinorversion").shouldNotHave(text(initialVerNumStr2)); //Verify that version has increased
+            $(elementLocators("InitialVersion")).shouldNotHave(text(initialVerNumStr2)); //Verify that version has increased
 
             $(By.id(FileUploadFieldTest.FileUploadFieldIds.numberField_minCount.name())).shouldHave(value(text_numberField_minCount));
 
@@ -122,12 +124,12 @@ public class FileUploadFieldTest extends BaseTest {
 
         //Enter Maximum Value
         if (StringUtils.isNotEmpty(text_numberField_maxCount)) {
-            String initialVerNumStr2 = $("#formMinorversion").should(exist).getText(); //Fetch initial version
+            String initialVerNumStr2 = $(elementLocators("InitialVersion")).should(exist).getText(); //Fetch initial version
             selectAndClear(By.id(FileUploadFieldIds.numberField_maxCount.name()))
                     .setValue(text_numberField_maxCount).sendKeys(Keys.TAB);
-            $("#formMinorversion").shouldNotHave(text(initialVerNumStr2)); //Verify that version has increased
+            $(elementLocators("InitialVersion")).shouldNotHave(text(initialVerNumStr2)); //Verify that version has increased
 
-            $("#numberField_maxCount").shouldHave(value(text_numberField_maxCount)).waitUntil(appears, 4000);
+            $(elementLocators("MaxCountInputField")).shouldHave(value(text_numberField_maxCount)).should(appear, Duration.ofSeconds(5));
 
             //Verify that if Max count is less than Min count, relevant errors should be shown:
             if(StringUtils.isNotEmpty(text_numberField_minCount)) {
@@ -135,7 +137,15 @@ public class FileUploadFieldTest extends BaseTest {
                 int int_text_numberField_maxCount = parseInt(text_numberField_maxCount);
                 if(int_text_numberField_minCount > int_text_numberField_maxCount){
                     String errorMaxCount1 = "The maximum value "+text_numberField_maxCount+" is less than minimum value "+text_numberField_minCount;
-                    $("#numberField_maxCount-helper-text").should(exist).shouldHave(text(errorMaxCount1));
+                    $(elementLocators("MaxCountErrorHelperText")).should(exist).shouldHave(text(errorMaxCount1));
+
+                    //Reset max value to valid value inorder to publish the form
+                    selectAndClear(By.id(FileUploadFieldTest.FileUploadFieldIds.numberField_maxCount.name()))
+                            .setValue(text_numberField_minCount).sendKeys(Keys.TAB);
+                    $(elementLocators("MaxCountInputField")).shouldHave(value(text_numberField_minCount));
+
+                    $(elementLocators("MaxCountErrorHelperText")).shouldNot(exist);
+
                 }
             }
 
@@ -146,22 +156,22 @@ public class FileUploadFieldTest extends BaseTest {
 
             String[] values = edit_values.split(",");
 
-            $("#formelement_properties_card .editForm").should(exist).click(); //Click on edit value pen icon
-            $("#form-value-list-card-dialog_content").should(exist); //Value List Editor window
+            $(elementLocators("EditValuesPenIcon")).should(exist).click(); //Click on edit value pen icon
+            $(elementLocators("ValuesEditor")).should(exist); //Value List Editor window
 
             //Deleting the existing rows:
-            List<SelenideElement> delBtn = $$("div.ag-pinned-right-cols-container .ag-row .fa-trash-alt");
-            int countDelBtn = $$("div.ag-pinned-right-cols-container .ag-row .fa-trash-alt").size();
+            List<SelenideElement> delBtn = $$(elementLocators("DeleteButtonsAvailable"));
+            int countDelBtn = $$(elementLocators("DeleteButtonsAvailable")).size();
             for (int n = countDelBtn; n >= 1; n--) {
-                String strDeleteBtn = ".ag-row:nth-child(" + n + ") .fa-trash-alt"; //Delete the n th row
+                String strDeleteBtn = "#myGrid .MuiDataGrid-row:nth-child(" + n + ") .fa-trash"; //Delete the n th row
                 $(strDeleteBtn).click();
-                $(strDeleteBtn).waitUntil(disappear, 10000);
+                $(strDeleteBtn).should(disappear, Duration.ofSeconds(10));
             }
 
             //Add rows in value list editor for the number of labels
-            if (!$("div.ag-pinned-right-cols-container .ag-row").exists()) {
+            if (!$(elementLocators("RowsInValuesEditor")).exists()) {
                 for (int x = 0; x < values.length; x++) {
-                    $("#form-value-list-card-dialog_content .fa-plus").should(exist).click();
+                    $(elementLocators("PlusIconToAddValues")).should(exist).click();
                 }
             }
 
@@ -172,22 +182,22 @@ public class FileUploadFieldTest extends BaseTest {
 
             for (int i = 1; i <= values.length; i++) {
                 //Click on label option
-                String labelSelector = "div.ag-body-viewport .ag-center-cols-viewport .ag-row:nth-child(" + i + ") .ag-cell:nth-child(2)";
+                String labelSelector = "#myGrid .MuiDataGrid-row:nth-child("+i+") div:nth-child(3)";
                 $(labelSelector).should(exist).doubleClick();
                 String labelValue = values[i - 1];
-                $("div.ag-popup input.ag-input-field-input").sendKeys(Keys.BACK_SPACE); //Clear the default value in label field
-                $("div.ag-popup input.ag-input-field-input").setValue(labelValue).sendKeys(Keys.ENTER);
+                $(elementLocators("LabelInputInEditor")).sendKeys(Keys.BACK_SPACE); //Clear the default value in label field
+                $(elementLocators("LabelInputInEditor")).setValue(labelValue).sendKeys(Keys.ENTER);
                 $(labelSelector).shouldHave(text(labelValue));
 
                 if (preselected.contains(labelValue)) {
-                    String checkboxSelector = "div.ag-pinned-left-cols-container .ag-row:nth-child(" + i + ") input";
+                    String checkboxSelector = "#myGrid .MuiDataGrid-row:nth-child("+i+") div:nth-child(1) span input";
                     $(checkboxSelector).should(exist).click();
                     $(checkboxSelector).shouldBe(checked);
                 }
             }
 
             //Click on close button
-            $("#form-value-list-card-dialog_actions #btnClosePropertiesForm").should(exist).click();
+            $(elementLocators("CloseValuesEditorBtn")).should(exist).click();
         }
 
     }
