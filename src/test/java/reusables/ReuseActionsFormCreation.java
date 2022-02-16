97a48c5c78006d56296f56a25d7ab435517aad3f
Khadar Sk <shaikh@visualorbit.com>
2022-02-16 15:22:43 +0530

    VLR-481: Updated CurrencyField and CheckboxgroupTests.

--- src/test/java/reusables/ReuseActionsFormCreation.java
+++ src/test/java/reusables/ReuseActionsFormCreation.java
@@ -258,8 +258,8 @@ public class ReuseActionsFormCreation {
         String initialVerNumStr1 = $(elementLocators("InitialVersion")).should(exist).getText(); //Fetch initial version
         selectAndClear(By.id(DesignFormFieldIds.textfield_label.name()))
                 .setValue(str_value).sendKeys(Keys.TAB);
-        $(elementLocators("InitialVersion")).shouldNotHave(text(initialVerNumStr1)); //Verify that version has increased
         $(StrBlockId).shouldHave(text(str_value));
+        $(elementLocators("InitialVersion")).shouldNotHave(text(initialVerNumStr1)); //Verify that version has increased
     }
 
     //Hide label
@@ -278,8 +278,8 @@ public class ReuseActionsFormCreation {
         String initialVerNumStr1 = $(elementLocators("InitialVersion")).should(exist).getText(); //Fetch initial version
         selectAndClear(By.id(DesignFormFieldIds.textfield_help.name()))
                 .setValue(str_value).sendKeys(Keys.TAB);
-        $(elementLocators("InitialVersion")).shouldNotHave(text(initialVerNumStr1)); //Verify that version has increased
         $(StrBlockId).shouldHave(text(str_value));
+        $(elementLocators("InitialVersion")).shouldNotHave(text(initialVerNumStr1)); //Verify that version has increased
     }
 
     //Required
