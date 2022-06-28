85e67514cfe3c03ddb8a2026ed19ae7a20d81249
Khadar Sk <shaikh@visualorbit.com>
2022-06-28 11:52:43 +0530

    VLR-511 Implemented ChecklistIntegrationByAssigningToAUserTest and did some modifications in ChecklistIntegrationWithoutAssigningTest

--- src/test/java/com/vo/checklist/ChecklistIntegrationByAssigningToAUserTest.java
+++ src/test/java/com/vo/checklist/ChecklistIntegrationByAssigningToAUserTest.java
@@ -1,28 +1,44 @@
 package com.vo.checklist;
 
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideConfig;
+import com.codeborne.selenide.SelenideDriver;
 import com.codeborne.selenide.SelenideElement;
 import com.vo.BaseTest;
+import org.apache.commons.lang3.tuple.Pair;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.Command;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.SessionId;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.Set;
 
 import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Configuration.browser;
 import static com.codeborne.selenide.Selectors.byAttribute;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static com.codeborne.selenide.WebDriverRunner.setWebDriver;
 import static reusables.ReuseActions.elementLocators;
 import static reusables.ReuseActionsChecklistCreation.createNewChecklist;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-@DisplayName("Verify the checklist Integration without assigning to a user")
-public class ChecklistIntegrationWithoutAssigningTest extends BaseTest {
+@DisplayName("Verify the checklist Integration with assigning to a user")
+public class ChecklistIntegrationByAssigningToAUserTest extends BaseTest {
 
     @Test
     @DisplayName("Create a new checklist and verify integration process")
     public void createNewChecklistAndVerifyIntegration() throws InterruptedException {
 
-        createNewChecklist();
+        createNewChecklist(); //Creates new checklist and opens the checklist designer
         SelenideElement sourceEventTrigger = $(elementLocators("SourceEventTrigger"));
         SelenideElement targetChecklistFlow = $(elementLocators("TargetChecklistFlow"));
 
@@ -41,23 +57,23 @@ public class ChecklistIntegrationWithoutAssigningTest extends BaseTest {
         $(elementLocators("FormsAvailableInTable")).shouldHave(text("IbanTestForm")).click(); //Select the first form available in the list
         $(elementLocators("TargetListInChecklistFlow")).shouldHave(text(selectedFormName1)); //Verify whether the selected form is available in the Checklist flow or not
         assert formDataID1 != null;
-        $(elementLocators("TargetListInChecklistFlow")).find(byAttribute("id",formDataID1)).should(exist);
+        $(elementLocators("TargetListInChecklistFlow")).find(byAttribute("id", formDataID1)).should(exist);
 
         //Drag n Drop Event Trigger to checklist flow
         actions().clickAndHold(sourceEventTrigger).moveToElement(targetChecklistFlow).build().perform();
-        int ETXOffset = (targetXOffset-sourceETXOffset)+50;
-        int ETYOffset=  (sourceETYOffset-targetYOffset)-250;
-        actions().moveByOffset(ETXOffset,ETYOffset).release().build().perform();
+        int ETXOffset = (targetXOffset - sourceETXOffset) + 50;
+        int ETYOffset = (sourceETYOffset - targetYOffset) - 250;
+        actions().moveByOffset(ETXOffset, ETYOffset).release().build().perform();
         $(elementLocators("EventTriggerEditor")).should(appear);
         $(elementLocators("EventTriggerEditor")).should(appear);
         $(elementLocators("IntegrationInputField")).should(exist).click();
         $(elementLocators("SecondIntegrationInTheList")).should(appear).click();
         SelenideElement Integration1 = $(elementLocators("IntegrationInputField")).should(exist).shouldHave(text("IBAN Number Verification"));
         String SelectedIntegration = Integration1.getText();
-        $("#sel_source").should(exist).click();
-        $("#menu- ul.MuiList-root").should(appear);
-        $("#menu- ul.MuiList-root  li:nth-child(2)").should(exist).click();
-        $("#sel_source").shouldHave(text("IbanTestForm > Iban"));
+        $(elementLocators("SourceSelectionField")).should(exist).click();
+        $(elementLocators("SourceList")).should(appear);
+        $(elementLocators("SecondSourceFromTheList")).should(exist).click();
+        $(elementLocators("SourceSelectionField")).shouldHave(text("IbanTestForm > Iban"));
         $(elementLocators("OkButton")).should(exist).click();
         $(elementLocators("TargetListInChecklistFlow")).shouldHave(text(SelectedIntegration)); //Verify whether the selected form is available in the Checklist flow or not
 
@@ -68,7 +84,7 @@ public class ChecklistIntegrationWithoutAssigningTest extends BaseTest {
         $(elementLocators("AttachFileIcon")).should(appear);
         $(elementLocators("FileUploadedMessage")).should(appear);
         $(elementLocators("SubmitButton")).click(); //Click on Submit Button
-       // $(elementLocators("CancelBtn")).should(exist).click(); //Click on Cancel Button
+        // $(elementLocators("CancelBtn")).should(exist).click(); //Click on Cancel Button
 
         SelenideElement Iban = $("#IBAN");
         SelenideElement Bic = $("#BIC");
@@ -99,8 +115,31 @@ public class ChecklistIntegrationWithoutAssigningTest extends BaseTest {
         $(byText("CheckList")).should(appear);
         $(elementLocators("ChecklistComponentList")).should(exist).shouldHave(text(selectedFormName1)).click();
         $(elementLocators("ChecklistData")).should(exist).shouldHave(text(selectedFormName1));
+        $(elementLocators("ChecklistComponentList")).should(exist).shouldHave(text(selectedFormName1)).$(byText("Assign to")).click();
+        $(elementLocators("UserSelectionInput")).should(appear);
+        $(elementLocators("DropDownButton")).should(exist).click();
+        $(elementLocators("Popover")).should(appear, Duration.ofSeconds(30));
+        $$(elementLocators("ListOfOptions")).findBy(text("GUI Tester")).should(appear, Duration.ofSeconds(30)).click();
+        $(elementLocators("UserSelectionInput")).shouldHave(value("GUI Tester")).pressTab();
+        $(byText("Assign")).should(exist).click();
+        $(elementLocators("ConfirmationMessage")).should(appear);
+        $(elementLocators("FormStateHelperText")).shouldHave(text("In Progress  |"));
+        $(elementLocators("CloseChecklistButton")).should(exist).click();
+        String versionID = $(elementLocators("FirstRowInTheGridContainer")).should(appear).getAttribute("data-id");
+        $(elementLocators("TaskStatus/Overview")).should(appear,Duration.ofSeconds(3)).shouldHave(text("0/1"));
+        assert versionID != null;
+        $(elementLocators("GridContainer")).find(byAttribute("data-id", versionID)).should(exist);
+
+        //Open the form with matching form name from MY Tasks table in the Launchpad in a new browser
+        Selenide.executeJavaScript("window.open()");
+        Selenide.switchTo().window(1);
+        open("https://visualorbit.fireo.net");
+        $(elementLocators("TasksCardInDashboard")).should(exist);
+        $(elementLocators("TasksCardInDashboard")).find(byAttribute("data-form-name", selectedFormName1 )).should(exist).
+                $(elementLocators("FillForm")).click();
+        $(elementLocators("DataFillForm")).should(appear);
         String validIban = "DE88 5005 0000 0001 0002 31";
-        $("#textField_form-user-b420757b-4c94-49ff-9c01-64bfb9afdd27").should(exist).sendKeys(validIban);
+        $("#textField_form-user-b420757b-4c94-49ff-9c01-64bfb9afdd27").should(exist).setValue(validIban);
         if(!$("#textField_form-user-b420757b-4c94-49ff-9c01-64bfb9afdd27").has(value(validIban))){
             $("#textField_form-user-b420757b-4c94-49ff-9c01-64bfb9afdd27").should(exist).sendKeys(validIban);
         }
@@ -108,10 +147,20 @@ public class ChecklistIntegrationWithoutAssigningTest extends BaseTest {
         $("#textField_form-user-b420757b-4c94-49ff-9c01-64bfb9afdd27").shouldHave(value(validIban));
         $("#textField_form-user-d379183d-295f-40ce-9e97-6232d595cf6c").shouldHave(value("HELADEFFXXX"));
         Thread.sleep(2000);
+        $(elementLocators("SubmitDataButton")).should(exist).click();
+        $(elementLocators("ConfirmButton")).should(appear).click();
+        $(elementLocators("TasksCardInDashboard")).find(byAttribute("data-form-name", selectedFormName1 )).should(disappear);
+
+        //Switch to default tab and verify the form state and do start workflow
+        Selenide.switchTo().window(0);
+        refresh();
+        $(elementLocators("GridContainer")).find(byAttribute("data-id", versionID)).should(exist)
+                .$(elementLocators("ShowChecklistButton")).click();
+        $(elementLocators("FormStateHelperText")).shouldHave(text("Completed  |  "));
         $(elementLocators("StartWorkFlowButton")).should(exist).click();
-        $(elementLocators("ConfirmationMessage")).should(appear).should(disappear);
+        $(elementLocators("ConfirmationMessage")).should(appear);
         $(byText("samplePDF.pdf")).click();
         $(byText("Loading PDF Preview")).should(disappear);
         $(elementLocators("IbanBlock")).shouldHave(text(validIban));
-      }
+    }
 }
